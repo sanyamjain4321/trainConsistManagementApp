e7ff6d4f6bd13961f6155ac5c1627d6493c0b38f
sanyamjain4321 <sanyamjainofficial4@gmail.com>
2026-03-29 15:21:35 +0530

    UC3

--- src/trainConsistManagementApp.java
+++ src/trainConsistManagementApp.java
@@ -1,52 +1,28 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class trainConsistManagementApp {
 
     public static void main(String[] args) {
 
-        // ================= UC1 =================
         System.out.println("========================================");
-        System.out.println("=== Train Consist Management App ===");
+        System.out.println("UC3 - Track Unique Bogie IDs");
         System.out.println("========================================\n");
 
-        List<String> trainConsist = new ArrayList<>();
+        Set<String> bogieIds = new LinkedHashSet<>();
 
-        System.out.println("Train initialized successfully...");
-        System.out.println("Initial Bogie Count : " + trainConsist.size());
-        System.out.println("Current Train Consist : " + trainConsist);
-        System.out.println("\nSystem ready for operations...\n");
+        bogieIds.add("BG104");
+        bogieIds.add("BG103");
+        bogieIds.add("BG102");
+        bogieIds.add("BG101");
+        bogieIds.add("BG101"); // duplicate
 
-        // ================= UC2 =================
-        System.out.println("========================================");
-        System.out.println("UC2 - Add Passenger Bogies to Train");
-        System.out.println("========================================\n");
+        System.out.println("Bogie IDs After Insertion:");
+        System.out.println(bogieIds + "\n");
 
-        // Create passenger bogie list
-        List<String> passengerBogies = new ArrayList<>();
+        System.out.println("Note:");
+        System.out.println("Duplicates are automatically ignored by HashSet.\n");
 
-        // Add bogies
-        passengerBogies.add("Sleeper");
-        passengerBogies.add("AC Chair");
-        passengerBogies.add("First Class");
-
-        System.out.println("After Adding Bogies:");
-        System.out.println("Passenger Bogies : " + passengerBogies + "\n");
-
-        // Remove AC Chair
-        passengerBogies.remove("AC Chair");
-
-        System.out.println("After Removing 'AC Chair':");
-        System.out.println("Passenger Bogies : " + passengerBogies + "\n");
-
-        // Check if Sleeper exists
-        System.out.println("Checking if 'Sleeper' exists:");
-        System.out.println("Contains Sleeper? : " + passengerBogies.contains("Sleeper") + "\n");
-
-        // Final list
-        System.out.println("Final Train Passenger Consist:");
-        System.out.println(passengerBogies + "\n");
-
-        System.out.println("UC2 operations completed successfully...");
+        System.out.println("UC3 uniqueness validation completed...");
     }
 }
